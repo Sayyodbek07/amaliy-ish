325ea8077713538aaebac5f64a1a1f4aba5742ae
Sayyodbek <sarvinoztogayeva603@gmail.com>
2025-07-01 13:33:58 +0500

    finish the task

---
+++ ghh.py
@@ -0,0 +1,14 @@
+talabalar = {
+    "Oquvchi 1": "Sayyodbek",
+    "Oquvchi 2": "Ziyobek",
+    "Oquvchi 3": "Shoxjahon"
+}
+
+natija = sorted(talabalar.values())
+print(natija)
+
+
+# 2-misol
+lst = ["a", "ccc", "dddd", "bb"]
+lst.sort(key=len)
+print(lst)
